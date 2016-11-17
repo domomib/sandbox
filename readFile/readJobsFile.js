2ce7812dfb97c9550736315107b8d9098d758b1c
domomib <micheldomotique@gmail.com>
2016-11-18 00:00:04 +0100

    update file

--- readFile/readJobsFile.js
+++ readFile/readJobsFile.js
@@ -3,49 +3,12 @@ var fs = require('fs');
 var sqlite3 = require("sqlite3").verbose();
 
 // Add debug function 
-var log = require('debug')('toto:log')
-var info = require('debug')('toto:info')
-var errorLog = require('debug')('toto:errorLog')
-/*var convert = function(index){
-	var value = "";
-	switch (index) {
-		case 0:
-			value = "Serveur Name";
-			break;
-		case 1:
-			value = "Domaine Name";
-			break;
-		case 2:
-			value = "Package"
-			break;
-		case 3:
-			value = "Version"
-			break;
-		case 4:
-			value = "Procedure"
-			break;
-		case 5:
-			value = "Status"
-			break;
-		case 6:
-			value = "Task"
-			break;
-		case 7:
-			value = "Activation time"
-			break;
-	}
-	return value;
-};*/
+var log = require('debug')('readJobsFile:log')
+var info = require('debug')('readJobsFile:info')
+var errorLog = require('debug')('readJobsFile:errorLog')
 
 // Manage database
 let dbFile = "./jobsEvents.db"
-var isDBExists = fs.existsSync(dbFile);
-
-if(!isDBExists) {
-  console.log("Creating DB file.");
-  fs.openSync(dbFile, "w");
-}
-var db = new sqlite3.Database(dbFile);
 
 var convertInt = function(int){
 	var a = String(parseInt(int));
@@ -59,30 +22,37 @@ var folder ='data';
 
 var readJobsInventories = new RSVP.Promise(function (resolve, reject){
 	var data = [];
+	// Get all job files
 	fs.readdir(folder, function(err, files){
+		// loop each files
 		for (var afile of files){
+			// Check if files is Software jobs
 			if (afile.search('Software_Jobs_By') !== -1) {
-
+				// Get date from  filename
 				var result = afile.match(/_(\d\d)(\d\d)(\d\d)_/);
-				var date = result[2] +'/' + convertInt(result[3] - 1) + '/20' + result[1];
+				
+			 	//var date = result[2] +'/' + convertInt(result[3] - 1) + '/20' + result[1];
+				var date = result[2] +'/' + convertInt(result[3]) + '/20' + result[1];
 
 				log("Read file "+ afile + "   for date: "+ date);
 
+				// handler of line reader
 				var lineReader = require('readline').createInterface({
 				 	input: fs.createReadStream(folder+'/' + afile)
 				});
 
+				// Line read Event
 				lineReader.on('line', function (line) {
-
 					if (line.search(date) != -1){
-						console.log(line);
-						var item = line.split(',');
-						var elems = item[0].split('.'); 
-						var time = item[7].split(' ');
-						data.push({'filename':afile, 'string':elems[1], 'server':elems[0], 'package':item[2], 'version':item[3], 'procedure':item[4], 'status':item[5], 'tack':item[6], 'date':time[0], 'time':time[1] });
+						info('date found');
+						var items = line.split(',');
+						var elems = items[0].split('.'); 
+						var time = items[7].split(' ');
+						data.push({'filename':afile, 'string':elems[1], 'server':elems[0], 'package':items[2], 'version':items[3], 'procedure':items[4], 'status':items[5], 'tack':items[6], 'date':time[0], 'time':time[1] });
 					}
 				});
 
+				// Close Event 
 				lineReader.on('close', function(){
 					log("List data provide to database");
 					log(data);
@@ -97,6 +67,7 @@ var readJobsInventories = new RSVP.Promise(function (resolve, reject){
 var updateDatabase = function(values){
 	log("Updating database in progress....");
 	db.serialize(function () {
+		// Create database if not exists
 		if (!isDBExists) {
 		 	db.run("CREATE TABLE jobs (string, server, package, version, procedure, status, tack, date, time, filename)");
 		 }
@@ -107,6 +78,7 @@ var updateDatabase = function(values){
 		 }
 		 stmt.finalize();
   
+  		// Check id data is present and display
 		db.each("SELECT * FROM jobs", function (err, row) {
 	    	if (err ===null){
 	    		log(row);
@@ -119,12 +91,22 @@ var updateDatabase = function(values){
 
 	});
 	//db.close();
-	
 };
+
+
+var isDBExists = fs.existsSync(dbFile);
+
+if(!isDBExists) {
+  console.log("Creating DB file.");
+  fs.openSync(dbFile, "w");
+}
+
+var db = new sqlite3.Database(dbFile);
+
 
 readJobsInventories.then(function(values){
 	log("readJobsInventories successfully executed")
-	//info(values);
+	info(values);
 	log("update database");
 	updateDatabase(values);
 
